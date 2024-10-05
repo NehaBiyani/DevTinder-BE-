77eb4d7fa88a90387a52b1ea2486de36b9651a04
NehaBiyani <Biyanineha307@gmail.com>
2024-10-05 11:06:17 +0530

    changes for middleware

--- src/app.js
+++ src/app.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
-app.use((req,res)=>{
-    res.send("Hello from the server")
+app.use('/admin',(req,res,next)=>{
+    const token='xyz';
+    console.log('token')
+    if(token==='xyz'){
+        next();
+    }
+    else{
+        res.status(401).send('Unauthorized Token')
+    }
+
 })
+
+app.get('/admin/data',(req,res)=>{
+    res.send({firstName:"Neha",lastName:"Biyani"});
+
+});
+app.post('/admin',(req,res)=>{
+    res.send("data saved successfully")
+})
+
 
 app.listen(3000,()=>{
     console.log("server is listening on port 3000")
